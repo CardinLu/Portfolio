f3978e8be78edb08d4f50677d41f00593ef22f97
CardinLu <156612158+CardinLu@users.noreply.github.com>
2024-01-21 22:18:23 -0600

    ;

--- basicType.js
+++ basicType.js
@@ -1,13 +1,13 @@
 /*Cardin 1/16/24*/
 var bob = 9;
 bob = 9 + bob;
-var karen = 5
 
 var age = prompt("What is your age?");
 
 console.log("Bob's value is " + bob);
 console.log("Your age is " + age);
 
+var karen = 5;
 for (let i = 0; i < 5; i++){
     if(karen % 2 !==0){
         console.log(karen)
