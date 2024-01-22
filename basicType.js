c33f17ae76fde139f04a355d6383fce3d494177e
CardinLu <156612158+CardinLu@users.noreply.github.com>
2024-01-21 22:33:32 -0600

    /

--- basicType.js
+++ basicType.js
@@ -1,15 +1,15 @@
 /*Cardin 1/16/24*/
-var bob = 9;
-bob = 9 + bob;
+// var bob = 9;
+// bob = 9 + bob;
 
-var age = prompt("What is your age?");
+// var age = prompt("What is your age?");
 
-console.log("Bob's value is " + bob);
-console.log("Your age is " + age);
+// console.log("Bob's value is " + bob);
+// console.log("Your age is " + age);
 
 var karen = 5;
 for (let i = 0; i < 5; i++){
-    if(karen % 2 != 0){
+    if(karen % 2 !== 0){
         console.log(karen)
     }
     karen++;
