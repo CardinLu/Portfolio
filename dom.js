87804dbac1b75afaf4b019575ea34229bc554d10
CardinLu <156612158+CardinLu@users.noreply.github.com>
2024-02-18 21:29:53 -0600

    w

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
 var bob = document.getElementById("bob");
 
-for (var i = 0; i < 5; i++) {
-    var paragraph = document.createElement("p");
-    paragraph.textContent = "Bob " + (i + 1);
+    for (var i = 0; i < 5; i++) {
+        var paragraph = document.createElement("p");
+        paragraph.textContent = "Bob " + (i + 1);
 
-    bob.appendChild(paragraph);
-}
+        bob.appendChild(paragraph);
+    }
 
 function changeColor() {
     document.getElementById('bob').style.color = "blue";
